4eb6777bd97e220390a927472db4c248aa2f13d5
Mujib <mujib@Personal-Macbook-Pro-14.local>
2025-07-29 15:17:09 +0530

    log for debuggin2

--- src/routes/healthRoutes.js
+++ src/routes/healthRoutes.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.status(200).json('Api running');
+});
+
+
 router.get('/health', (req, res) => {
     res.status(200).json({ status: 'ok' });
 });
