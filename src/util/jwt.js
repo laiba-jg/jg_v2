12d476b37a0775cf111c3e6c878b5cf51f7d8e19
dev_lt <dev@example.com>
2025-08-28 15:46:33 +0500

    jwt token decode

--- src/util/jwt.js
+++ src/util/jwt.js
@@ -17,3 +17,10 @@ export const generateToken = (payload) => {
 export const generateServiceToken = (payload) => {
     return jwt.sign(payload, process.env.JWT_SECRET);
 };
+
+export const decodeToken = (token) => jwt.decode(token);
+
+// verifies + decodes
+export const getTokenInfo = (token) => {
+   return jwt.verify(token, process.env.JWT_SECRET); 
+};
